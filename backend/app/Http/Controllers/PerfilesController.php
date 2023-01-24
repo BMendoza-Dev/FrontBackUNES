a8cf978f02e369d876222fe86b75fa73bc7c4182
GuidoAdrian97 <guidoadrian97@hotmail.com>
2023-01-24 00:21:30 -0500

    optencion api

--- backend/app/Http/Controllers/PerfilesController.php
+++ backend/app/Http/Controllers/PerfilesController.php
@@ -17,23 +17,11 @@ class PerfilesController extends Controller
 
         $token = $tokenapi->json();
 
-
-$headers = [
-  'Content-Type' => 'application/jason',
-  'Authorization' => 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiI2ODU2NkQ1OTcxMzM3NDM2NzczOTdBMjQ0MzI2NDYyOTQ4NDA0RDYzNTE2NjU0NkE1NzZFNUE3MjM0NzUzNzc4MjE0MTI1NDQyQTQ3MkQ0QjYxNTA2NDUyNjc1NTZCNTg3MDMyNzMzNTc2Mzg3OTJGNDIzRjQ1Mjg0ODJCNEQ2MiIsInNjb3BlIjoicmVhZCIsImlhdCI6MTY3NDUzMTExOCwiZXhwIjoxNjc0NTMyOTE4fQ.prokX0NtpoWFAzKhjoHk0Yk-RTMAsPJ8tZXbUws4V3n7sGt3pkxau3BgAgWIxueHpSnWE5ENrwnVm03JzCDBVQ'
-];
-
-$response = Http::withHeaders($headers)->get('http://apiapp.asambleanacional.gob.ec/periodsResource/findActivePeriod');
-//$request = new Http::get('http://apiapp.asambleanacional.gob.ec/periodsResource/findActivePeriod', $headers);
-
-      /*  $response = Http::withHeaders([
+        $request = Http::withHeaders([
             'Content-Type' => 'application/jason',
-            'Authorization' => $token['token']
-        ])->get('http://apiapp.asambleanacional.gob.ec/periodsResource/findActivePeriod');*/
-
-   // $response = Http::withToken($token['token'])->post('http://apiapp.asambleanacional.gob.ec/periodsResource/findActivePeriod');
-     //  dd($data);
-     dd($response);
-        return   $response->json()  ;
+          'Authorization' => $token['token'],
+        ])->get('http://apiapp.asambleanacional.gob.ec/periodsResource/findActivePeriod');
+        dd($request);
+        return  $request->json()  ;
     }
 }
