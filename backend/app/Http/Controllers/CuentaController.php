572779199e8e93f585ed67aea2ca76535b301f9a
GuidoAdrian97 <guidoadrian97@hotmail.com>
2023-01-26 12:12:17 -0500

    arreglado

--- backend/app/Http/Controllers/CuentaController.php
+++ backend/app/Http/Controllers/CuentaController.php
@@ -15,7 +15,7 @@ class CuentaController extends Controller
      */
     public function index()
     {
-        $datos=Cuenta::with('rol')->get();
+        $datos=Cuenta::where('id',2)->with('rol')->get();
         $num_rows = count($datos);
         if($num_rows!=0){
            return response()->json($datos);
