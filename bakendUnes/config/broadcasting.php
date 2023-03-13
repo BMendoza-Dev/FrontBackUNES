2fd06771a8b003a0a4295e21f89aa19128c487d3
GuidoAdrian97 <guidoadrian97@hotmail.com>
2023-03-13 03:16:50 -0500

    regreso websockets

--- bakendUnes/config/broadcasting.php
+++ bakendUnes/config/broadcasting.php
@@ -36,7 +36,7 @@ return [
             'secret' => env('PUSHER_APP_SECRET'),
             'app_id' => env('PUSHER_APP_ID'),
             'options' => [
-                'host' => '54.88.90.157',
+                'host' => '127.0.0.1',
                 'port' => 6001,
                 'scheme' => 'http',
                 'encrypted' => false,
