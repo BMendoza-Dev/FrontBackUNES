5e8c7799907cb6ed67bb0b909d2af1fe8e3722c4
GuidoAdrian97 <guidoadrian97@hotmail.com>
2023-03-13 23:49:54 -0500

    websockets arreglando

--- bakendUnes/config/broadcasting.php
+++ bakendUnes/config/broadcasting.php
@@ -40,7 +40,7 @@ return [
                 'port' =>  6001,
                 'scheme' => 'http',
                 'encrypted' => false,
-                'useTLS' => 'false'
+                'useTLS' => false
             ],
             'client_options' => [
                 // Guzzle client options: https://docs.guzzlephp.org/en/stable/request-options.html
