9a759b19499deaf20eae6c4402568cf4f8226c03
GuidoAdrian97 <guidoadrian97@hotmail.com>
2023-03-14 23:21:27 -0500

    websocket aparentemente arreglado

--- bakendUnes/config/broadcasting.php
+++ bakendUnes/config/broadcasting.php
@@ -36,14 +36,12 @@ return [
             'secret' => env('PUSHER_APP_SECRET'),
             'app_id' => env('PUSHER_APP_ID'),
             'options' => [
-                'host' => '127.0.0.1',
-                'port' =>  6001,
-                'scheme' => 'http',
+                'cluster' => env('PUSHER_APP_CLUSTER'),
+                'useTLS' => false,
                 'encrypted' => false,
-                'useTLS' => false
-            ],
-            'client_options' => [
-                // Guzzle client options: https://docs.guzzlephp.org/en/stable/request-options.html
+                'host' => '127.0.0.1',
+                'port' => 6001,
+                'scheme' => 'http'
             ],
         ],
 
