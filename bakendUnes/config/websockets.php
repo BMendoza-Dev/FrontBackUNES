525c61c3d5ede38e52a19b47d78ba60547099fe0
GuidoAdrian97 <guidoadrian97@hotmail.com>
2023-03-16 20:55:50 -0500

    arreglando

--- bakendUnes/config/websockets.php
+++ bakendUnes/config/websockets.php
@@ -24,12 +24,12 @@ return [
     'apps' => [
         [
             'id' => env('PUSHER_APP_ID'),
-            'name' => env('APP_NAME'),
+            'name' => 'realtime-apps',
             'key' => env('PUSHER_APP_KEY'),
             'secret' => env('PUSHER_APP_SECRET'),
             'path' => env('PUSHER_APP_PATH'),
             'capacity' => null,
-            'enable_client_messages' => true,
+            'enable_client_messages' => false,
             'enable_statistics' => true,
         ],
     ],
