1ecb151918244451cf0f088b4e60dd4a09ed653f
GuidoAdrian97 <guidoadrian97@hotmail.com>
2023-03-17 21:15:57 -0500

    chat provando

--- bakendUnes/app/Events/ChatEvent.php
+++ bakendUnes/app/Events/ChatEvent.php
@@ -26,7 +26,7 @@ class ChatEvent implements ShouldBroadcast
     {
         //
         $this->message = $message;
-        $this->from = auth()->user()->name;
+       // $this->from = auth()->user()->name;
     }
 
     /**
--- bakendUnes/config/broadcasting.php
+++ bakendUnes/config/broadcasting.php
@@ -37,14 +37,13 @@ return [
             'app_id' => env('PUSHER_APP_ID'),
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
-                'encrypted' => true,
                 'host' => '127.0.0.1',
+                'useTLS' => false,
+                'encrypted' => false,
                 'port' => 6001,
                 'scheme' => 'http'
             ],
-            'client_options' => [
-                // Guzzle client options: https://docs.guzzlephp.org/en/stable/request-options.html
-            ],
+
         ],
 
         'ably' => [
