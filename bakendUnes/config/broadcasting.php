fc7252850e43ea8c8f8a060a70a5175f41a59cca
GuidoAdrian97 <guidoadrian97@hotmail.com>
2023-03-21 20:15:52 -0500

    arreglado blodcaster

--- bakendUnes/config/broadcasting.php
+++ bakendUnes/config/broadcasting.php
@@ -37,7 +37,7 @@ return [
             'app_id' => env('PUSHER_APP_ID'),
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
-                'host' => 'rc5appmobile.tech',
+                'host' => '127.0.0.1',
                 'useTLS' => true,
                 'encrypted' => true,
                 'port' => 6001,
