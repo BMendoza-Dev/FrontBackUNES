a1d364f2e39e454dcfac49f226af89cc3214f471
GuidoAdrian97 <guidoadrian97@hotmail.com>
2023-03-23 02:05:02 -0500

    wss

--- bakendUnes/config/broadcasting.php
+++ bakendUnes/config/broadcasting.php
@@ -38,10 +38,10 @@ return [
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
                 'host' => '127.0.0.1',
-                'useTLS' => false,
+                'useTLS' => true,
                 'encrypted' => true,
                 'port' => 6001,
-                'scheme' => 'http'
+                'scheme' => 'https'
             ],
 
         ],
