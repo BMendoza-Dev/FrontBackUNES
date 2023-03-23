f8be0fa5f28f669db55912d5ff46c0eaf7f1c952
GuidoAdrian97 <guidoadrian97@hotmail.com>
2023-03-23 00:26:23 -0500

    arreglando web soket

--- bakendUnes/config/broadcasting.php
+++ bakendUnes/config/broadcasting.php
@@ -38,11 +38,12 @@ return [
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
                 'host' => '127.0.0.1',
-                'useTLS' => false,
-                'encrypted' => false,
+                'useTLS' => true,
+                'encrypted' => true,
                 'port' => 6001,
-                'scheme' => 'http'
+                'scheme' => 'https'
             ],
+
         ],
 
         'ably' => [
