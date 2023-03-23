d24a5f19fd94987c1b16b9e598d9980f215853ec
GuidoAdrian97 <guidoadrian97@hotmail.com>
2023-03-22 22:13:33 -0500

    wss 10 prueaba

--- bakendUnes/config/broadcasting.php
+++ bakendUnes/config/broadcasting.php
@@ -40,7 +40,7 @@ return [
                 'host' => '127.0.0.1',
                 'useTLS' => true,
                 'encrypted' => true,
-                'port' => 443,
+                'port' => 6001,
                 'scheme' => 'https',
                 'curl_options' => [
                     CURLOPT_SSL_VERIFYHOST => 0,
