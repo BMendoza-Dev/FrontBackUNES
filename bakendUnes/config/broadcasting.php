fd2158cd05d334bc71dcbc55da1a59e74f2acc4f
GuidoAdrian97 <guidoadrian97@hotmail.com>
2023-03-26 23:22:35 -0500

    arreglando laravel wss2

--- bakendUnes/config/broadcasting.php
+++ bakendUnes/config/broadcasting.php
@@ -38,10 +38,10 @@ return [
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
                 'host' => '127.0.0.1',
-                'useTLS' => true,
-                'encrypted' => true,
-                'port' => 443,
-                'scheme' => 'http'
+               // 'useTLS' => false,
+               // 'encrypted' => false,
+                'port' => 6001,
+                'scheme' => 'https'
             ],
 
         ],
