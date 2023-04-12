214b3aca1da6772a8650ce4e0d12887ff524a8d6
GuidoAdrian97 <guidoadrian97@hotmail.com>
2023-04-12 00:10:55 -0500

    tiempo time zone7

--- bakendUnes/config/database.php
+++ bakendUnes/config/database.php
@@ -62,7 +62,6 @@ return [
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
-            'timezone' => 'America/Guayaquil'
         ],
 
         'pgsql' => [
