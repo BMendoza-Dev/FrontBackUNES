3ac2a335cc9d6286219d026a2a6eea57a1e20c5b
GuidoAdrian97 <guidoadrian97@hotmail.com>
2023-04-12 01:02:40 -0500

    tiempo time zone18

--- bakendUnes/app/Http/Controllers/BlogsController.php
+++ bakendUnes/app/Http/Controllers/BlogsController.php
@@ -194,8 +194,9 @@ class BlogsController extends Controller
      public function AprobarBlogEnUltimaNoticias(Request $request){
         $blog =  Blog::findOrFail($request->id);
         $fechaHora1 = Carbon::parse($blog->updated_at);
-        $fechaHora2 = Carbon::parse(Carbon::createFromFormat('Y-m-d H:i:s', $request->updated_at, 'America/Guayaquil'));
         var_dump(DateTimeZone::listIdentifiers(DateTimeZone::ALL));
+        $fechaHora2 = Carbon::parse(Carbon::createFromFormat('Y-m-d H:i:s', $request->updated_at, 'America/Guayaquil'));
+        
         if (!$fechaHora1->equalTo($fechaHora2)) {
             return ['error'=>'500', 'menssaje'=>'El Blog no pudo ser actualizado debido a que el creador lo ha modificado'];
         } 
