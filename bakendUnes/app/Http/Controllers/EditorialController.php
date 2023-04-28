1750f7dfebd1fa575b591c5cb607987aba5a18a3
GuidoAdrian97 <guidoadrian97@hotmail.com>
2023-04-28 01:32:26 -0500

    commit prueba 4

--- bakendUnes/app/Http/Controllers/EditorialController.php
+++ bakendUnes/app/Http/Controllers/EditorialController.php
@@ -57,7 +57,10 @@ class EditorialController extends Controller
 
     // Sincroniza las relaciones de blogs con los nuevos IDs proporcionados
 
-    $editorial->blogs()->sync($blogsid);
+    //$editorial->blogs()->sync($blogsid);
+    foreach ($blogsid as $position => $blogid) {
+        $editorial->blogs()->attach($blogid, ['position' => $position]);
+    }
 
     return response()->json('Editorial actualizado con éxito');
       }
