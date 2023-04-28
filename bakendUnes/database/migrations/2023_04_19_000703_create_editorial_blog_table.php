863ab22f275c853d25adb38fa2cf45ffd5bcd7bb
GuidoAdrian97 <guidoadrian97@hotmail.com>
2023-04-28 00:23:13 -0500

    commit time editorial

--- bakendUnes/database/migrations/2023_04_19_000703_create_editorial_blog_table.php
+++ bakendUnes/database/migrations/2023_04_19_000703_create_editorial_blog_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('blog_editorial', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->timestamps(7);
             $table->foreignId('editorial_id')->references('id')->on('editorials');
             $table->foreignId('blog_id')->references('id')->on('blogs');
         });
