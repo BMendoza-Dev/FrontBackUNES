aeb94e1f44a356515cafb1ee12b3a0e856abb1ef
GuidoAdrian97 <guidoadrian97@hotmail.com>
2023-05-20 00:53:43 -0500

    channel appmobile4

--- bakendUnes/app/Models/MobileUser.php
+++ bakendUnes/app/Models/MobileUser.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Models;
-
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Laravel\Sanctum\HasApiTokens;
