cb735583b50b178eb7246ac5332fa1c8a993be28
GuidoAdrian97 <guidoadrian97@hotmail.com>
2023-05-20 01:05:33 -0500

    channel appmobile6

--- bakendUnes/app/Models/MobileUser.php
+++ bakendUnes/app/Models/MobileUser.php
@@ -5,10 +5,10 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Laravel\Sanctum\HasApiTokens;
-use Illuminate\Database\Eloquent\SoftDeletes;
+
 class MobileUser extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable,SoftDeletes;
+    use HasApiTokens, HasFactory, Notifiable;
 
     protected $table = 'mobile_users';
     protected $guarded = ['id'];
