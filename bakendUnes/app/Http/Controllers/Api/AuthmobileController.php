b0a7ba86d7f26017fb2681fb1fb322c2b82aaa80
GuidoAdrian97 <guidoadrian97@hotmail.com>
2023-05-21 22:46:48 -0500

    channel appmobile16

--- bakendUnes/app/Http/Controllers/Api/AuthmobileController.php
+++ bakendUnes/app/Http/Controllers/Api/AuthmobileController.php
@@ -93,7 +93,7 @@ class AuthmobileController extends Controller
             ];
 
         });
-        return $notify[0]->TipeNotify;
+        return $notify;
         event(new EventNotifyUsersApp($notify,$notify->TipeNotify,Auth::user()->id));
     }
 }
